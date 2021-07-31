2ba00f001616b16d44bcddda5756c62f026492fd
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-07-31 16:50:30 +0430

    V1.1

---
+++ DataPreparation.py
@@ -0,0 +1,28 @@
+from tensorflow.keras import backend as K
+from matplotlib import pyplot as plt
+import tensorflow as tf
+import numpy as np
+
+
+def create_serial_dataset(x=None, y=None, n=100, length=10, frames=10):
+
+    X = np.zeros([n, length*frames, x.shape[1], x.shape[2]])
+    Y = np.zeros([n, length*frames, 10])
+
+    for i in range(n):
+
+        k = np.random.randint(0, 1000, size=(length))
+        for j in range(length):
+          
+            X[i, j*frames:j*frames+frames, :, :] = x[k[j], :, :]
+            Y[i, j*frames:j*frames+frames, y[k[j]]] = 1
+          
+    return X.reshape(n, length*frames, x.shape[1] * x.shape[2]), Y
+
+def init_MNIST():
+
+    (x_train,y_train), (x_test,y_test) = tf.keras.datasets.mnist.load_data()
+    Xn, Yn = create_serial_dataset(x_train, y_train, n=100, length=10, frames=20)
+    X = Xn[:10, :, :]
+    Y = Yn[:10, :, :]
+    return X, Y, Xn, Yn
