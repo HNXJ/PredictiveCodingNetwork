bdc8b8265cc4940de143d5fd0fc965dc36a0398b
Hamed Nejat <59973245+HNXJ@users.noreply.github.com>
2021-08-02 17:16:58 +0430

    V1.4

--- DataPreparation.py
+++ DataPreparation.py
@@ -15,14 +15,16 @@ def create_serial_dataset(x=None, y=None, n=100, length=10, frames=10):
         for j in range(length):
           
             X[i, j*frames:j*frames+frames, :, :] = x[k[j], :, :]
-            Y[i, j*frames:j*frames+frames, y[k[j]]] = 1
+            Y[i, j*frames-1:j*frames+frames-1, y[k[j]]] = 1
           
     return X.reshape(n, length*frames, x.shape[1] * x.shape[2]), Y
 
 def init_MNIST():
 
     (x_train,y_train), (x_test,y_test) = tf.keras.datasets.mnist.load_data()
-    Xn, Yn = create_serial_dataset(x_train, y_train, n=100, length=10, frames=20)
+    Xn, Yn = create_serial_dataset(x_train, y_train, n=100, length=10, frames=10)
+    Xn /= 255.0
+    Yn /= 255.0
     X = Xn[:10, :, :]
     Y = Yn[:10, :, :]
     return X, Y, Xn, Yn
